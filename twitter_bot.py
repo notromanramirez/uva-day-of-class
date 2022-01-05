55ca88d1fe51ad355f974963e3da3753b7b4643a
notromanramirez <justromanramirez@gmail.com>
2022-01-05 16:33:50 -0500

    Bug fixes.

--- twitter_bot.py
+++ twitter_bot.py
@@ -12,10 +12,10 @@ import os
 
 # create environment variables
 load_dotenv()
-CONSUMER_KEY = os.environ["CONSUMER_KEY"]
-CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
-ACCESS_KEY = os.environ["ACCESS_KEY"]
-ACCESS_SECRET = os.environ["ACCESS_SECRET"]
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+ACCESS_KEY = os.getenv("ACCESS_KEY")
+ACCESS_SECRET = os.getenv("ACCESS_SECRET")
 
 ### SETTING UP TWITTER API ###
 
